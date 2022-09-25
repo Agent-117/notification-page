a981dd4045025eed6b33aace7311e5fb2ebd03a3
Tunji Taiwo <taiwoadetunji117@gmail.com>
2022-09-25 02:16:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const markRead = document.querySelector('#mark');
+const num = document.querySelector('#notif-num');
+const the = document.querySelector('span');
+
+function read() {
+  const unread = document.querySelectorAll('.unread');
+  const circle = document.querySelectorAll('.circle');
+
+
+  for(let markAll of unread) {
+    markAll.classList.remove('unread');
+  }
+
+  for(let cir of circle) {
+    cir.classList.remove('circle');
+  }
+
+  num.removeChild(the)
+  num.append(0)
+}
+
+markRead.addEventListener('click', read);
